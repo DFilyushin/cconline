48a8d9e517d96893b2516dbcb570e5c04908bf79
DFilyushin <dmitriy.filyushin@yandex.ru>
2015-12-29 17:51:32 +0600

    автоматическая загрузка данных при выборе страницы

--- static/js/app.js
+++ static/js/app.js
@@ -30,3 +30,15 @@ nurseApp.config(['$routeProvider',
         redirectTo: '/lab'
       });
   }]);
+
+nurseApp.directive('afterRender', ['$timeout', function ($timeout) {
+    var def = {
+        restrict: 'A',
+        terminal: true,
+        transclude: false,
+        link: function (scope, element, attrs) {
+            $timeout(scope.$eval(attrs.afterRender), 0);  //Calling a scoped method
+        }
+    };
+    return def;
+}]);
