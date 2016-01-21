2396d6c5b244e0f55eb81752a33afb1374582457
DFilyushin <dmitriy.filyushin@yandex.ru>
2016-01-21 12:30:24 +0600

    + добавлено выполнение назначений + кнопка возврата + вывод вчера/сегодня/завтра

--- static/js/controllers/nurseController.js
+++ static/js/controllers/nurseController.js
@@ -9,15 +9,22 @@ var vikingApp=angular.module('vikingApp').config(function($httpProvider) {
 vikingApp.controller('NurseController',
     function NurseController($scope, $http) {
         $scope.id_history = 0;
-        $scope.selectedTemplate=null;
-        $scope.tableVisible = false;
-        $scope.isCito = false;
-        $scope.dayView = 'today';
+        $scope.datavalue = {};
+        $scope.datavalue.mode = 'today';
         $scope.nurseWork = [];
+        $scope.dates = [
+            {value: 'yesterday', label: 'Вчера'},
+            {value: 'today', label: 'Сегодня'},
+            {value: 'tomorrow', label: 'Завтра'}
+        ];
 
         $scope.init = function(id)
         {
             $scope.id_history = id;
+        };
+
+        $scope.setDateValue = function(val){
+          $scope.datavalue.mode = val;
         };
 
         $scope.getRefreshData = function(){
@@ -25,14 +32,23 @@ vikingApp.controller('NurseController',
                 {
                     url: "/json/nurse/",
                     method: "GET",
-                    params: {p: $scope.dayView, id: $scope.id_history}
+                    params: {period: $scope.datavalue.mode, id: $scope.id_history}
                 })
                     .success(function(data){
                         $scope.nurseWork = data;
                 }
                 );
         };
+        $scope.executeNurse = function (idx, typeAssign) {
+            $http(
+                {
+                    url: "/json/nurse_execute/",
+                    method: 'POST',
+                    data: { t: typeAssign, id:$scope.nurseWork[idx].pk  }
+                });
+            $scope.nurseWork.splice(idx, 1);
 
+        };
 
 
     });
